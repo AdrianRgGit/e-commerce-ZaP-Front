696b464382379c6f94bdaba6ee482bc3566ab0b1
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-08-05 10:19:33 +0200

    añadido antd

--- src/components/Users/Login/Login.jsx
+++ src/components/Users/Login/Login.jsx
@@ -1,49 +1,81 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../../../context/UserContext/UserState";
 import { useNavigate } from "react-router-dom";
+import { Button, Form, Input, notification } from "antd";
 
 const Login = () => {
   const { login } = useContext(UserContext);
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  
+
   const navigate = useNavigate();
 
-  const handleInputChange = (event) => {
-    setUser({
-      ...user,
-      [event.target.name]: event.target.value,
+  const onSubmit = (values) => {
+    login(values);
+    notification.success({
+      message: "Successfully logged",
     });
-  };
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    login(user);
-    navigate("/")
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
   };
 
   return (
-    <form>
-      <input
-        type="email"
-        className="email"
-        placeholder="Email"
-        name="email"
-        onChange={handleInputChange}
-      />
-      <input
-        type="password"
-        className="password"
-        placeholder="Password"
-        name="password"
-        onChange={handleInputChange}
-      />
-      <div className="submit-container">
-        <input type="submit" onClick={onSubmit} />
-      </div>
-    </form>
+    <div>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        onFinish={onSubmit}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+            {
+              type: "email",
+              message: "Please input a correct email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
