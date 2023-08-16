92bb66c957b1e584aa4117a624a53662fcaf2120
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-08-16 14:23:54 +0200

    ya se ve la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-
 import { UserProvider } from "./context/UserContext/UserState";
-// import Users from "./components/Users/Users";
+import Users from "./components/Users/Users";
 import Home from "./components/Home/Home";
 import Login from "./components/Users/Login/Login";
 import Logout from "./components/Users/Logout/Logout";
 import Header from "./components/Header/Header";
-import SignUp from "./components/Users/SignUp/SignUp";
-import { ProductProvider } from "./context/ProductContext/ProductState";
-import Products from "./components/Home/Products/Products";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <UserProvider>
-          <ProductProvider>
-            {/* Header */}
-            <Header />
+          {/* Header */}
+          <Header />
+          
+          {/* Home */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
 
-            {/* Home */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-
-            {/* Users */}
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/signup" element={<SignUp />} />
-            </Routes>
-          </ProductProvider>
+          {/* Users */}
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
         </UserProvider>
       </BrowserRouter>
     </>
