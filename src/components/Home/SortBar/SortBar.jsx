0a6a849a8b807a693558a6f1975ba1272395cd97
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-08-30 18:10:28 +0200

    luego sigo

--- src/components/Home/SortBar/SortBar.jsx
+++ src/components/Home/SortBar/SortBar.jsx
@@ -30,12 +30,19 @@ const SortBar = () => {
     <>
       <div className="sort-container">
         <div className="button-container">
-          <button className="asc-button" onClick={showProductsAsc}>
-            Asc
-          </button>
-          <button className="desc-button" onClick={showProductsDesc}>
-            Desc
-          </button>
+          <img
+            className="arrow"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAU1JREFUSEvt1c8qRVEUx/HvHaEkypyBB5CSSPmTIg9gbOYl7tArmJubC8mfkhLJCxAPYEDyZ6ZfrV234/xZe59yJ/uMbueusz9r//Y6nQ59ujp9csnwvyXfJupB6/I7pdtUeAA4MnAD+InFU+Ah4BBYNuwC2AS+YvBYWOgpsFBAroG1GDwGHgaOS9DQg/B14MOzcy8s9ASYt0VfgImS327cA49YvLMGXQG7heHqAov2/63F/l638yZY6CUw3YMqTjWhodKlIRN2BszZvQdgCajE6+AxWyygN8Aq8GnYuSEr1oSOQ4PnwuvgceDOzvLedhAGR7sswupDuBKaAR6tideyyJuingL2gC3grWeBKlglo8ABsAM8VZ1zE1z1XB3seZuSP4sZDq+WK2YV5TP2RjUJbFvxPvDsfTDUpUYd6/ypz3DrCL0L5Ki9SbWu+wW6A0IfgGQJ4gAAAABJRU5ErkJggg=="
+            onClick={showProductsAsc}
+          />
+
+          <span className="span-text">Sort by price</span>
+          
+          <img
+            className="arrow"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAWRJREFUSEvt1d9KFVEUx/HPuQzSiO5F6AVEiFKCVAojCHoEew+fwGfwwjfwTjREE0SoIHoAL7TrQCxJRC9iyRoYDufMmTlHOzezb2b2zFq/716//a9jTK0zJq4W/N+cb62ua/U0VjJ4Ayd1E4u4Ya1ewH6KLOJzCx7kQGv1IIeK//e+uJ5iHR9wXhpVFfgRNvERp/0qqZrjJ/iOqXy+wkUK9QM/xAFmcYw5/OoFrwI/xh5mMvELlvAXvcAB3cXzjP+R8WdNwRE/mRUU8EO8wYuuA+RrDrIMDYd+D2N1kRPwqORZfggX1rCd/bdYxcvsf8PrKmjE1d3HE/iUlUZenM1xXkf7mesg3o8QA/kzaHvUBYdOzOEO5vuIBnS5tAAr2U3AIfQgbe+GxyXxDpeDKh3lduqGBzTsvaoLbTLH3ZoB38I13jeFjgIubL9JeJNib2ObznFjwCj7+M5gZaG24nuxtZfoP5n8RB/nmy5uAAAAAElFTkSuQmCC"
+            onClick={showProductsDesc}
+          />
         </div>
         <div className="selector-container">
           <label htmlFor="brand"></label>
